44b9008b1fffbdc0bd7d9ec66f3fcdb538ec2d37
Hoaison3108 <hoaison3108@gmail.com>
2025-02-13 17:01:03 +0700

    update

---
+++ 5.Bai5-BIEN-JS/main5.2.js
@@ -0,0 +1,16 @@
+/*
+Bài tập 1: Viết chương trình, hiện thông báo lên màn hình, cho phép người dùng nhập tên
+-> Xuất tên dùng console.log
+*/
+
+//Khởi tạo biến và Hiển thị hộp thoại nhập liệu đồng thời gán dữ liệu
+let userName = prompt("Vui lòng nhập tên của bạn: ");
+console.log(userName);
+console.log("Tên của bạn là: " + userName);
+
+/*
+- Tên biến có thể bắt đầu bằng chữ cái, dấu gạch dưới_ hoặc $ 
+- JS phân biệt chữ hoa và chữ thường
+- Tránh sử dụng các từ khóa có sẵn trong JS làm tên biến
+- Tuân theo quy ước đặt tên biến theo kiểu camelCase
+ */
---
+++ 6.Bai6-KIEU DU LIEU JS/main.js
@@ -0,0 +1,29 @@
+/* 
+Primitive Data Types - Kiểu dữ liệu nguyên thủy trong JS
+1) String : Kiểu chuỗi - kiểu dữ liệu để lưu trữ chuỗi ký tự
+2) Number : Kiểu số - Kiểu dữ liệu để lưu trữ số, có thể là số nguyên hoặc số thực
+3) Boolean : Kiểu lý luận -  chỉ có thể nhận giá trị true hoặc false
+4) Undefined : Biến chưa được gán giá trị
+5) Null : Biến được gán giá trị là null, thường được dùng để reset biến hoặc gán giá trị mặc định
+
+*/
+
+// 1) Kiểu String
+let myString = `abc`;
+console.log(myString);
+//Kiểm tra kiểu biến dữ liệu
+console.log(typeof myString);
+
+//2) Kiểu Number
+let soNguyen = 10;
+let soThuc = 3.14;
+//xuất giá trị
+console.log(soNguyen);
+console.log(typeof soNguyen);
+
+console.log(soThuc);
+console.log(typeof soThuc);
+
+//3) Kiểu Boolean
+let check = true;
+console.log(typeof check);
